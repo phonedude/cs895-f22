0235a41ce8f3b9071925283eef41450d440f1817
lesleyodu <77700615+lesleyodu@users.noreply.github.com>
2022-11-19 20:10:01 -0500

    filter query timemaps

--- assignments/frew/week-14-extending-edgi/params.py
+++ assignments/frew/week-14-extending-edgi/params.py
@@ -1,6 +1,9 @@
 from urllib.parse import urlparse
 import pandas as pd
 import json
+import sys
+import os
+from surt import handyurl
 
 def alphabetize_params(url):
     o = urlparse(url)
@@ -11,7 +14,7 @@ def filter_timemap(timemap):
     try:
         jso = pd.read_json(timemap)
     except:
-        return
+        return ""
     
     urif = alphabetize_params(jso.loc['first']['original_uri'])
     
@@ -21,6 +24,9 @@ def filter_timemap(timemap):
         uric = alphabetize_params(memento['uri'])
         if uric == urif:
             mementos2.append(memento)
+    
+    if len(mementos2) == 0:
+        return ""
     
     json2 = {}
     json2['original_url'] = jso.loc['first']['original_uri']
@@ -37,8 +43,29 @@ def filter_timemap(timemap):
     json2['timemap_uri']['cdxj_format'] = prefix + 'cdxj/' + json2['original_url']
     json2['timegate_uri'] = jso.loc['first']['timegate_uri']
     
-    print(json.dumps(json2, indent = 2))
+    return json.dumps(json2, indent = 2)
 
-filter_timemap('00051.json')
-    
+i = 1
+query_urirs = []
+with open('get_timemaps.txt') as urls:
+  for line in urls:
+        urir = handyurl.parse(line.strip()).geturl()
+        if '?' in urir or '&' in urir:
+            url_form = '{:0>5}'.format(i) + '.json'
+            query_urirs.append(url_form)
+        i = i + 1
+
+original_stdout = sys.stdout
+x = 1
+
+for timemap in os.listdir('timemaps'+ str(x)):
+  #timemap = '00051.json'
+  if timemap in query_urirs:
+    tmap2 = filter_timemap("timemaps" + str(x) + "/" + timemap)
+    with open("filtered-timemaps/" + timemap, 'w') as f:
+      sys.stdout = f # Change the standard output to the file we created.
+      if tmap2:
+          print(tmap2)
+      sys.stdout = original_stdout # Reset the standard output to its original value
+
     
